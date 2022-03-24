88e8721ed1d238e7de9a659f4faa383467dc9cb5
HoodVincy <vincent.guilhoto@hoodrbains.com>
2022-03-24 22:00:11 +0100

    feature(lbcApi) : add public api module definition

---
+++ lbcapi/api/src/main/kotlin/com/hoodbrains/lbcapi/LbcApi.kt
@@ -0,0 +1,6 @@
+package com.hoodbrains.lbcapi
+
+interface LbcApi {
+
+    suspend fun fetchItemList(): List<LbcItem>
+}
---
+++ lbcapi/api/src/main/kotlin/com/hoodbrains/lbcapi/LbcItem.kt
@@ -0,0 +1,9 @@
+package com.hoodbrains.lbcapi
+
+data class LbcItem(
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String
+)
