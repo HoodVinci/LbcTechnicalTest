39921597c45e7c8af5093609f8d63e191513b0be
HoodVincy <vincent.guilhoto@hoodrbains.com>
2022-03-28 01:25:57 +0200

    feature(persistence) :  adapt use case to save data

--- domain/src/test/kotlin/com/hoodbrains/lbctest/domain/usecases/FetchItemListUseCaseTest.kt
+++ domain/src/test/kotlin/com/hoodbrains/lbctest/domain/usecases/FetchItemListUseCaseTest.kt
@@ -7,9 +7,11 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.verifyNoMoreInteractions
 import org.mockito.kotlin.whenever
 
 
@@ -36,13 +38,25 @@ class FetchItemListUseCaseTest {
     }
 
     @Test
-    fun `on remote success do not call local return success`() = runTest {
+    fun `on remote success only save locally, return success`() = runTest {
         whenever(remoteRepository.fetchList()).thenReturn(listOf())
 
         val result = fetchItemListUseCase.run()
 
-        verifyNoInteractions(localRepository)
+        verify(localRepository).storeList(listOf())
+        verifyNoMoreInteractions(localRepository)
         assertThat(result.isSuccess).isTrue()
+    }
+
+    @Test
+    fun `on remote success save to local, on save error ,return success`() = runTest {
+        whenever(remoteRepository.fetchList()).thenReturn(listOf())
+        whenever(localRepository.storeList(any())).thenThrow(NullPointerException())
+
+        val result = fetchItemListUseCase.run()
+
+        assertThat(result.isSuccess).isTrue()
+
     }
 
     @Test
